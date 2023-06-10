96694e73290bd7e9c2e2b888184ef80d59c2853f
rautyrauty <rautyrauty@gmail.com>
2023-06-10 15:49:32 +0300

    Create sv_anti-family-sharing.lua

---
+++ garrysmod/lua/autorun/server/sv_anti-family-sharing.lua
@@ -0,0 +1,25 @@
+local webApiKey = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+local function CheckFamilySharing(ply)
+	http.Fetch
+    (
+        string.format(
+            "https://partner.steam-api.com/ISteamUser/CheckAppOwnership/v2/?key=%s&steamid=%s&appid=4000",
+            webApiKey,
+            ply:SteamID64()
+        ),
+
+        function(body)
+            local body = util.JSONToTable(body)
+            
+            if (not util.JSONToTable(body).permanent) then
+                ply:Kick("You kicked by AntiFamilySharing")
+            end
+        end,
+
+        function(code)
+            error(string.format("FamilySharing: Failed API call for %s | %s (Error: %s)\n", ply:Nick(), ply:SteamID(), code))
+        end
+	)
+end
+hook.Add("PlayerAuthed", "CheckFamilySharing", CheckFamilySharing)
