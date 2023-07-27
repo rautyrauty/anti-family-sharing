1d78d00dd1d4347ec5e46d5b7c21b8fe8e53ec22
rautyrauty <rautyrauty@gmail.com>
2023-07-27 12:40:43 +0500

    Update sv_anti-family-sharing.lua

--- garrysmod/lua/autorun/server/sv_anti-family-sharing.lua
+++ garrysmod/lua/autorun/server/sv_anti-family-sharing.lua
@@ -1,34 +1,8 @@
-local webApiKey = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-
-local function CheckFamilySharing(ply)
-    require "gthread"
-
-    local thread = gthread.newThread()
-
-    thread:Run
-    ( 
-        function()
-            http.Fetch
-            (
-                    string.format
-                    (
-                        "https://partner.steam-api.com/ISteamUser/CheckAppOwnership/v2/?key=%s&steamid=%s&appid=4000",
-                        webApiKey,
-                        ply:SteamID64()
-                    ),
-
-                    function(body)
-                        if (not util.JSONToTable(body).permanent) then
-                            ply:Kick("You kicked by AntiFamilySharing")
-                        end
-                    end,
-
-                    function(code)
-                        error(string.format("FamilySharing: Failed API call for %s | %s (Error: %s)\n", ply:Nick(), ply:SteamID(), code))
-                    end
-            )
-        end 
+require("AntiFamilySharing")
+hook.Add(
+    "PlayerAuthed", 
+    "CheckFamilySharing", 
+    function(ply)
+        LuaAntiFamilySharing(ply:SteamID64(),ply:UserID())
+    end
     )
-	
-end
-hook.Add("PlayerAuthed", "CheckFamilySharing", CheckFamilySharing)
